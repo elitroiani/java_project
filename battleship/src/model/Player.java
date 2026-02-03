28f60a6ebc43129e55db6c8df2ec63d030bea6a8
elitroiani <e.troiani1@campus.uniurb.it>
2026-02-03 16:59:04 +0100

    Modified Player (Interface) adding some methods

--- battleship/src/model/Player.java
+++ battleship/src/model/Player.java
@@ -1,7 +1,16 @@
 package model;
 
+import java.awt.Point;
+
 public interface Player {
 
 	Grid getGrid();
-
+	String getName();
+	
+	/**
+     * Restituisce la prossima mossa.
+     * Per HumanPlayer la mossa è fornita dal Controller.
+     * Per AIPlayer la mossa è generata dall'IA.
+     */
+	Point chooseMove();  // ritorna la prossima mossa
 }
